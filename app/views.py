4cad7373af828a2dba55211061f1efd5cbe1c60a
Fig Newtons <dev.dangruszczynski@gmail.com>
2016-01-16 15:17:59 -0500

    Crappy menu, two templates, and iterative content

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'FigNewtons'}
+
+    items = [
+        {
+            'product': {'name': 'Hat 1'},
+            'description': 'Baby blue hat'
+        },
+        {
+            'product': {'name': 'Hat 2'},
+            'description': 'Pink hat'
+        }
+    ]
+
+    return render_template('index.html',
+                            title = 'My Store',
+                            user = user,
+                            items = items)
